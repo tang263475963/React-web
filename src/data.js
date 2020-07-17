cfa3487d186068d625d65baff3828b4f0f5d3de3
tang263475963 <tang263475963@gmail.com>
2020-07-17 09:15:13 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,28 @@
+export default{
+    products: [
+        {
+            id: 1,
+            name: 'CPU',
+            brand: 'Intel',
+            price: 6500,
+            rating: 4.5,
+            numStock: 50,
+            numReview: 10,
+            image: 'images/p1.jpg'
+        },
+
+        {
+            id: 2,
+            name: 'RAM',
+            brand: 'corsair',
+            price: 3000,
+            rating: 4.7,
+            numStock: 20,
+            numReview: 13,
+            image: 'images/p2.jpg'
+        }
+        
+
+    ]
+
+}
